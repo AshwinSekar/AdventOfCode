c97bbaa94be232dedd01569a3e60ae8a1bcccfdd
Ashwin Sekar <asekar@andrew.cmu.edu>
2019-03-16 16:11:32 -0400

    Add switcher for days

--- 2018/src/Main.hs
+++ 2018/src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified ChronalCalibration as CC
 import qualified InventoryManagement as IM
+import System.IO (hFlush, stdout)
 
 readLines :: [String] -> IO [String]
 readLines lines = do
@@ -10,8 +11,22 @@ readLines lines = do
       then return $ reverse lines
       else readLines (line:lines)
 
+part1 :: Int -> [String] -> IO ()
+part1 1 = print . CC.part1
+part1 2 = print . IM.part1
+
+part2 :: Int -> [String] -> IO ()
+part2 1 = print . CC.part2
+part2 2 = print .IM.part2
 
 main :: IO ()
 main = do
+  putStr "Day?: "
+  hFlush stdout
+  day <- getLine
   input <- readLines []
-  print $ CC.part1 input
+  putStrLn "----- Part 1: -----"
+  part1 (read day) input
+  putStrLn ""
+  putStrLn "----- Part 2: -----"
+  part2 (read day) input
