5bec6461b42493a850cd9cfd3ee6ebc0f811eb79
Ashwin Sekar <gaurjas@gmail.com>
2020-05-22 01:05:31 -0400

    Solve 2019 Day 2 part 2

--- 2019/src/2-1202-program-alarm.hs
+++ 2019/src/2-1202-program-alarm.hs
@@ -1,39 +1,45 @@
+import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
+
 import Data.Array.ST
+import Data.List
 
 import Utils
-import Debug.Trace
 
 main :: IO ()
 main = do
   putStrLn "Input:"
   input <- getLine
   let inputs = splitString (== ',') input
-      p1 = runST $ part1 $ map read inputs
+      intprog = map read inputs
+      p1 = runST $ runProgram intprog (12, 2)
+      results = map (\x -> (x, runST $ runProgram intprog x)) $ liftA2 (,) [0..99] [0..99]
+      Just ((noun, verb), _) = find ((== 19690720) . snd) results
+      p2 = 100 * noun + verb
   putStrLn $ "Part 1: " ++ show p1
+  putStrLn $ "Part 2: " ++ show p2
 
-part1 :: [Int] -> ST s Int
-part1 input = do
+runProgram :: [Int] -> (Int, Int) -> ST s Int
+runProgram input (noun, verb) = do
   prog <- newListArray (0, length input) input :: ST s (STArray s Int Int)
-  -- 1202 alarm modifications
-  writeArray prog 1 12
-  writeArray prog 2 2
+  -- noun verb modifications
+  writeArray prog 1 noun
+  writeArray prog 2 verb
   -- evaluate the program
-  runProgram prog 0
+  runProgram' prog 0
   readArray prog 0
 
-runProgram :: STArray s Int Int -> Int -> ST s ()
-runProgram prog i = do
+
+runProgram' :: STArray s Int Int -> Int -> ST s ()
+runProgram' prog i = do
   opcode <- readArray prog i
   unless (opcode == 99) $ do
-    p1 <- readArray prog (i + 1)
-    p2 <- readArray prog (i + 2)
+    a <- readArray prog =<< readArray prog (i + 1)
+    b <- readArray prog =<< readArray prog (i + 2)
     p3 <- readArray prog (i + 3)
     let op = case opcode of
               1 -> (+)
               2 -> (*)
-    a <- readArray prog p1
-    b <- readArray prog p2
     writeArray prog p3 $ a `op` b
-    runProgram prog (i + 4)
+    runProgram' prog (i + 4)
