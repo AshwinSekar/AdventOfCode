c10c2d3b9fd924a36041ea39d1769a82356cdb26
Ashwin Sekar <gaurjas@gmail.com>
2020-05-28 02:59:05 -0400

    Rework 2019 Day 7 part 1 with state monad

--- 2019/src/7-amplification-circuit.hs
+++ 2019/src/7-amplification-circuit.hs
@@ -1,105 +1,162 @@
 import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
+import Control.Monad.Trans.State.Lazy
+import Control.Monad.Trans.Class
 
 import Data.Array.ST
 import Data.List
+import Debug.Trace
+
 
 import Utils
+
+
+-- progs, instruction pointers, inputs, phase, currentAmp
+data ProgState s = ProgState { ips :: STArray s Int Integer,
+                               amps :: [IntProg s],
+                               currentAmp :: Int,
+                               inputs :: [Integer],
+                               phase :: [Integer]
+                             }
+type IntProg s = STArray s Integer Integer
+type StateWithOutputs s = StateT (ProgState s) (ST s) [Integer]
+type IntOp = (Integer -> Integer -> Integer)
+type IntCond = (Integer -> Integer -> Bool)
+
 
 main :: IO ()
 main = do
   putStrLn "Input:"
   input <- getLine
   let inputs = splitString (== ',') input
-      intprog = map read inputs
-      phases = permutations [0,1,2,3,4]
-      maxThrust = maximum $ map (findThrust intprog) phases
+      prog = map read inputs
+      p1Phases = permutations [0..4]
+      maxThrust = maximum $ map (findThrust prog) p1Phases
   putStrLn $ "Part 1: " ++ show maxThrust
   -- putStrLn $ "Part 2: " ++ show p2
 
+findThrust :: [Integer] -> [Integer] -> Integer
+findThrust prog phase = head $ runST (runProgram prog phase)
 
-findThrust :: [Int] -> [Int] -> Int
-findThrust intprog = foldl' (thrust intprog) 0
 
-thrust :: [Int] -> Int -> Int -> Int
-thrust intprog out phase =
-  let [signal] = runST $ runProgram intprog [phase, out]
-  in signal
+-- intprog state
+shift offset instr = (instr `div` place) `rem` 10
+  where place = 10 ^ (offset + 1)
 
-runProgram :: [Int] -> [Int] -> ST s [Int]
-runProgram intprog inputs = do
-  prog <- newListArray (0, length intprog) intprog :: ST s (STArray s Int Int)
+getPmode offset = do
+  i <- getIP
+  prog <- getProg
+  lift $ shift offset <$> readArray prog i
+
+{-# ANN readParam "Hlint: ignore Reduce duplication" #-}
+readParam :: Integer -> StateT (ProgState s) (ST s) Integer
+readParam offset = do
+  i <- getIP
+  prog <- getProg
+  pmode <- getPmode offset
+  lift $ case pmode of
+          0 -> readArray prog >=> readArray prog $ i + offset
+          1 -> readArray prog (i + offset)
+
+readParams :: StateT (ProgState s) (ST s) (Integer, Integer)
+readParams = liftM2 (,) (readParam 1) (readParam 2)
+
+{-# ANN writeParam "Hlint: ignore Reduce duplication" #-}
+writeParam :: Integer -> Integer -> StateT (ProgState s) (ST s) ()
+writeParam offset val = do
+  prog <- getProg
+  i <- (+ offset) <$> getIP
+  c <- lift $ readArray prog i
+  void $ lift (writeArray prog c val)
+
+getIP :: StateT (ProgState s) (ST s) Integer
+getIP = do is <- gets ips
+           c <- gets currentAmp
+           lift $ readArray is c
+
+getProg :: StateT (ProgState s) (ST s) (STArray s Integer Integer)
+getProg = liftM2 (!!) (gets amps) (gets currentAmp)
+
+incIP x = ((+ x) <$> getIP) >>= putIP
+
+putIP :: Integer -> StateT (ProgState s) (ST s) ()
+putIP i = do
+  is <- gets ips
+  c <- gets currentAmp
+  lift $ writeArray is c i
+
+putInputs xs = modify (\s -> s {inputs = xs})
+putCurAmp c = modify (\s -> s {currentAmp = c})
+
+runProgram :: [Integer] -> [Integer] -> ST s [Integer]
+runProgram intprog phase = do
+  progs <- mapM (\_ -> newArray (0, 4096) 0) [0..4]
+  mapM_ (populate intprog) progs
+  is <- newArray (0, 5) 0
   -- evaluate the program
-  runProgram' prog inputs 0
+  evalStateT runProgram' $ ProgState is progs 0 inputs phase
+  where inputs = [head phase, 0]
+
+populate :: [Integer] -> STArray s Integer Integer -> ST s [()]
+populate intprog prog = zipWithM (writeArray prog) [0..] intprog
 
 
-runProgram' :: STArray s Int Int -> [Int] -> Int -> ST s [Int]
-runProgram' prog inputs i = do
-  instr <- readArray prog i
+runProgram' :: StateWithOutputs s
+runProgram' = do
+  prog <- getProg
+  ip <- getIP
+  instr <- lift $ readArray prog ip
   case instr `rem` 100 of
-    1 -> plusMult prog inputs i
-    2 -> plusMult prog inputs i
-    3 -> input prog inputs i
-    4 -> output prog inputs i
-    5 -> jump prog inputs i
-    6 -> jump prog inputs i
-    7 -> cond prog inputs i (<)
-    8 -> cond prog inputs i (==)
-    99 -> return []
+    1 -> plusMult (+)
+    2 -> plusMult (*)
+    3 -> input
+    4 -> output
+    5 -> jump (/=)
+    6 -> jump (==)
+    7 -> cond (<)
+    8 -> cond (==)
+    99 -> lift $ return []
+    _ -> fail ("Unexpected instr: " ++ show instr)
 
-getPmodes instr =
-  let pmodes = instr `div` 100
-  in (pmodes `rem` 10, (pmodes `div` 10) `rem` 10)
+plusMult :: IntOp -> StateWithOutputs s
+plusMult op = do
+  (a, b) <- readParams
+  writeParam 3 $ a `op` b
+  incIP 4
+  runProgram'
 
-getParams prog i = do
-  instr <- readArray prog i
-  let (pa, pb) = getPmodes instr
-  a <- readArray prog (i + 1)
-  b <- readArray prog (i + 2)
-  a <- if pa == 0 then readArray prog a else return a
-  b <- if pb == 0 then readArray prog b else return b
-  return (a,b)
+input :: StateWithOutputs s
+input = do
+  (x:xs) <- gets inputs
+  writeParam 1 x
+  putInputs xs
+  incIP 2
+  runProgram'
 
-plusMult :: STArray s Int Int -> [Int] -> Int -> ST s [Int]
-plusMult prog inputs i = do
-  -- find parameter mode
-  opcode <- (`rem` 100) <$> readArray prog i
-  (a, b) <- getParams prog i
-  c <- readArray prog (i + 3)
-  let op = case opcode of
-            1 -> (+)
-            2 -> (*)
-  writeArray prog c $ a `op` b
-  runProgram' prog inputs (i + 4)
+output :: StateWithOutputs s
+output = do
+  a <- readParam 1
+  nextAmp <- (+1) <$> gets currentAmp
+  incIP 2
+  if nextAmp > 4 then (a:) <$> runProgram' else do
+    nextPhase <- (!! nextAmp) <$> gets phase
+    putCurAmp nextAmp
+    putInputs [nextPhase, a]
+    runProgram'
 
-input :: STArray s Int Int -> [Int] -> Int -> ST s [Int]
-input prog (x:xs) i = do
-  a <- readArray prog (i + 1)
-  writeArray prog a x
-  runProgram' prog xs (i + 2)
+jump :: IntCond -> StateWithOutputs s
+jump p = do
+  i <- getIP
+  (a, b) <- readParams
+  let i' = if p a 0 then b else i + 3
+  putIP i'
+  runProgram'
 
-output :: STArray s Int Int -> [Int] -> Int -> ST s [Int]
-output prog inputs i = do
-  -- find parameter mode
-  (pa, _) <- getPmodes <$> readArray prog i
-  a <- readArray prog (i + 1)
-  a <- if pa == 0 then readArray prog a else return a
-  (a:) <$> runProgram' prog inputs (i + 2)
-
-jump :: STArray s Int Int -> [Int] -> Int -> ST s [Int]
-jump prog inputs i = do
-  opcode <- (`rem` 100) <$> readArray prog i
-  (a, b) <- getParams prog i
-  let cond = case opcode of
-              5 -> a /= 0
-              6 -> a == 0
-  if cond then runProgram' prog inputs b else runProgram' prog inputs (i + 3)
-
-cond :: STArray s Int Int -> [Int] -> Int -> (Int -> Int -> Bool) -> ST s [Int]
-cond prog inputs i p = do
-  (a, b) <- getParams prog i
+cond :: IntCond -> StateWithOutputs s
+cond p = do
+  (a, b) <- readParams
   let val = if p a b then 1 else 0
-  c <- readArray prog (i + 3)
-  writeArray prog c val
-  runProgram' prog inputs (i + 4)
+  writeParam 3 val
+  incIP 4
+  runProgram'
