765fe8fb86fefb5a0b45634f172154c85734f9fc
Ashwin Sekar <gaurjas@gmail.com>
2020-05-29 23:06:50 -0400

    Solve 2019 Day 13

--- 2019/src/13-care-package.hs
+++ 2019/src/13-care-package.hs
@@ -1,51 +1,98 @@
 import Control.Applicative
+import Control.Concurrent
 import Control.Monad
-import Control.Monad.ST
+import Control.Monad.ST.Trans
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans.Class
 
 import Data.Array.ST
 import Data.List
+import Data.Maybe
+
+import Debug.Trace
 
 import System.IO
+import System.Console.ANSI
 
 import Utils
 
 -- prog, instruction pointer, relative base, inputs
-data ProgState = ProgState { ip :: Integer,
-                             base :: Integer,
-                             inputs :: [Integer]
-                           } deriving (Show)
+data ProgState s = ProgState { ip :: Integer,
+                               base :: Integer,
+                               outputs :: [Integer],
+                               key :: MVar Char,
+                               inputF :: InputF s,
+                               ballx :: Integer,
+                               paddlex :: Integer
+                             }
 type IntProg s = STArray s Integer Integer
-type StateWithOutputs s = StateT ProgState (ST s) [Integer]
+type GameState s = StateT (ProgState s) (STT s IO) ()
 type IntOp = (Integer -> Integer -> Integer)
 type IntCond = (Integer -> Integer -> Bool)
+type InputF s = StateT (ProgState s) (STT s IO) Integer
+
+liftIO :: IO a -> StateT (ProgState s) (STT s IO) a
+liftIO = lift . lift
 
 main :: IO ()
 main = do
   input <- filter (/= '\n') <$> readFile "data/13-puzzle-input"
   let inputs = splitString (== ',') input
-      intprog = map read inputs
-      initBoard = runST $ runProgram intprog [1]
-      p1 = findBlocks initBoard
-  putStrLn $ "Part 1: " ++ show p1
-  -- putStrLn $ "Part 2: " ++ show p2
+      intprog = map read inputs :: [Integer]
+  key <- newEmptyMVar
+  initBoard <- runSTT $ runProgram intprog key getKey
+  clearScreen >> setCursorPosition 0 0
+  putStrLn $ "Part 1: " ++ show (findBlocks initBoard)
+  playGame (2 : drop 1 intprog) key False
+
+
+playGame :: [Integer] -> MVar Char -> Bool -> IO ()
+playGame prog key manual = do
+  putStrLn "Part 2 - Score: "
+  hideCursor
+  if manual
+    then setupKeypress >> forkIO getKeypress
+    else myThreadId
+  runSTT $ runProgram prog key (if manual then getKey else getAi)
+  setCursorPosition 40 0
+  putStrLn "Game Over"
+  where getKeypress = getChar >>= putMVar key >> getKeypress
+        setupKeypress = hSetBuffering stdin NoBuffering >> hSetEcho stdin False
+
+displayBoard = gets outputs >>= displayBoard'
+
+displayBoard' :: [Integer] -> GameState s
+displayBoard' [] = return ()
+displayBoard' (s:0:(-1):xs) = do
+  liftIO $ setCursorPosition 1 17
+  liftIO $ putStr (show s)
+  displayBoard' xs
+displayBoard' (t:y:x:xs) = do
+  tile <- case t of
+              0 -> return ' '
+              1 -> return '▓'
+              2 -> return '■'
+              3 -> putPaddleX x >> return '='
+              4 -> putBallX x >> return 'o'
+  liftIO $ setCursorPosition (fromIntegral y + 2) (fromIntegral x)
+  liftIO $ putChar tile
+  displayBoard' xs
 
 findBlocks :: [Integer] -> Int
 findBlocks board = length $ filter (== 2) tileIds
   where tileIds = map fst $ filter third (zip board [1..])
-        third = (== 0) . (`rem` 3) . snd
+        third = (== 1) . (`rem` 3) . snd
 
-runProgram :: [Integer] -> [Integer] -> ST s [Integer]
-runProgram intprog inputs = do
+runProgram :: [Integer] -> MVar Char -> InputF s -> STT s IO [Integer]
+runProgram intprog key input = do
   prog <- newArray (0, 4096) 0
   zipWithM_ (writeArray prog) [0..] intprog
-  let initState = ProgState 0 0 inputs
+  let initState = ProgState 0 0 [] key input 0 0
   -- evaluate the program
-  evalStateT (runProgram' prog) initState
+  outputs <$> execStateT (runProgram' prog >> displayBoard) initState
 
 
-runProgram' :: IntProg s -> StateWithOutputs s
+runProgram' :: IntProg s -> GameState s
 runProgram' prog = do
   i <- gets ip
   instr <- lift $ readArray prog i
@@ -59,7 +106,7 @@ runProgram' prog = do
     7 -> cond prog (<)
     8 -> cond prog (==)
     9 -> adjBase prog
-    99 -> lift $ return []
+    99 -> return ()
 
 shift offset instr = (instr `div` place) `rem` 10
   where place = 10 ^ (offset + 1)
@@ -70,7 +117,7 @@ getPmode prog offset = do
   lift $ shift offset <$> readArray prog i
 
 {-# ANN readParam "Hlint: ignore Reduce duplication" #-}
-readParam :: IntProg s -> Integer -> StateT ProgState (ST s) Integer
+readParam :: IntProg s -> Integer -> StateT (ProgState s) (STT s IO) Integer
 readParam prog offset = do
   i <- gets ip
   bp <- gets base
@@ -80,11 +127,11 @@ readParam prog offset = do
           1 -> readArray prog (i + offset)
           2 -> readArray prog =<< ((+ bp) <$> readArray prog (i + offset))
 
-readParams :: IntProg s -> StateT ProgState (ST s) (Integer, Integer)
+readParams :: IntProg s -> StateT (ProgState s) (STT s IO) (Integer, Integer)
 readParams prog = liftM2 (,) (readParam prog 1) (readParam prog 2)
 
 {-# ANN writeParam "Hlint: ignore Reduce duplication" #-}
-writeParam :: IntProg s -> Integer -> Integer -> StateT ProgState (ST s) ()
+writeParam :: IntProg s -> Integer -> Integer -> StateT (ProgState s) (STT s IO) ()
 writeParam prog offset val = do
   i <- gets ip
   bp <- gets base
@@ -96,31 +143,59 @@ writeParam prog offset val = do
 
 
 putIP i = modify (\s -> s {ip = i})
-putInput xs = modify (\s -> s {inputs = xs})
 putBase b = modify (\s -> s {base = b})
+putOutputs o = modify (\s -> s {outputs = o})
+putPaddleX x = modify (\s -> s {paddlex = x})
+putBallX x = modify (\s -> s {ballx = x})
 
-plusMult :: IntProg s -> IntOp -> StateWithOutputs s
+plusMult :: IntProg s -> IntOp -> GameState s
 plusMult prog op = do
   (a, b) <- readParams prog
   writeParam prog 3 $ a `op` b
   gets ip >>= putIP . (+4)
   runProgram' prog
 
-input :: IntProg s -> StateWithOutputs s
+getKey :: InputF s
+getKey = do
+  -- get input
+  k <- wait
+  (liftIO . threadDelay) 500000
+  return $ case k of
+            'a' -> -1
+            'd' -> 1
+            _ -> 0
+
+getAi :: InputF s
+getAi = do
+  paddleX <- gets paddlex
+  ballX <- gets ballx
+  (liftIO . threadDelay) 50000
+  return $ case compare paddleX ballX of
+            LT -> 1
+            EQ -> 0
+            GT -> -1
+
+input :: IntProg s -> GameState s
 input prog = do
-  (x:xs) <- gets inputs
-  writeParam prog 1 x
-  putInput xs
+  -- display board
+  displayBoard >> putOutputs []
+  liftIO $ hFlush stdout
+  writeParam prog 1 =<< (join . gets) inputF
   gets ip >>= putIP . (+2)
   runProgram' prog
 
-output :: IntProg s -> StateWithOutputs s
+wait =  do
+  k <- (liftIO . tryTakeMVar) =<< gets key
+  maybe ((liftIO . threadDelay) 500000 >> wait) return k
+
+output :: IntProg s -> GameState s
 output prog = do
   a <- readParam prog 1
   gets ip >>= putIP . (+2)
-  (a:) <$> runProgram' prog
+  gets outputs >>= putOutputs . (a:)
+  runProgram' prog
 
-jump :: IntProg s -> IntCond -> StateWithOutputs s
+jump :: IntProg s -> IntCond -> GameState s
 jump prog p = do
   i <- gets ip
   (a, b) <- readParams prog
@@ -128,7 +203,7 @@ jump prog p = do
   putIP i'
   runProgram' prog
 
-cond :: IntProg s -> IntCond -> StateWithOutputs s
+cond :: IntProg s -> IntCond -> GameState s
 cond prog p = do
   (a, b) <- readParams prog
   let val = if p a b then 1 else 0
@@ -136,7 +211,7 @@ cond prog p = do
   gets ip >>= putIP . (+4)
   runProgram' prog
 
-adjBase :: IntProg s -> StateWithOutputs s
+adjBase :: IntProg s -> GameState s
 adjBase prog = do
   a <- readParam prog 1
   gets base >>= putBase . (+a)
