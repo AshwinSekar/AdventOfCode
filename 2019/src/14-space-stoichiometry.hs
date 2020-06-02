3cf10cf96dbc4e82bec5cf67bb2ca2d96a825d73
Ashwin Sekar <gaurjas@gmail.com>
2020-06-02 01:45:31 -0400

    Solve 2019 Day 14 part 2

--- 2019/src/14-space-stoichiometry.hs
+++ 2019/src/14-space-stoichiometry.hs
@@ -3,30 +3,34 @@ import Control.Monad
 import Control.Monad.State.Lazy
 import Data.List
 import qualified Data.Map as Map
+import Data.Ratio
+import Data.Tuple.Extra
 
 import Utils
 import Debug.Trace
 
 type Edges = Map.Map String [(String, (Integer, Integer))] -- u -> [(vi, (xi, yi))] where each xi vi = yi u + ...
-type ChemicalState = (Map.Map String Integer, [String])
+type ChemicalState = (Map.Map String Integer, Map.Map String Rational, [String])
 
 main :: IO ()
 main = do
   putStrLn "Input:"
   input <- readLines []
   let edges = Map.fromListWith (++) $ concatMap parse input
-      (p1, p2) = evalState (chemistry edges) (Map.empty, [])
+      (p1, ratio) = evalState (chemistry edges) (Map.empty, Map.empty, [])
+      trillRatio = 1000000000000 % 1 / ratio
+      p2 = numerator trillRatio `div` denominator trillRatio
   putStrLn $ "Part 1: " ++ show p1
   putStrLn $ "Part 2: " ++ show p2
 
 
-chemistry :: Edges -> State ChemicalState (Integer, Integer)
+chemistry :: Edges -> State ChemicalState (Integer, Rational)
 chemistry edges = do
   topSort edges
-  l <- reverse <$> gets snd
+  l <- reverse <$> gets thd3
   mapM_ (computeQuantity edges) l
-  p1 <- (Map.! "ORE") <$> gets fst
-  p2 <- computeFuel edges 1000000000000
+  p1 <- (Map.! "ORE") <$> gets fst3
+  p2 <- (Map.! "ORE") <$> gets snd3
   return (p1, p2)
 
 
@@ -44,7 +48,7 @@ topSort edges =
   let
     dfs :: String -> State ChemicalState ()
     dfs s = do
-      visited <- gets fst
+      visited <- gets fst3
       case (Map.member s visited, edges Map.!? s) of
         (True, _) -> return ()
         (_, Nothing) -> addTop s
@@ -52,26 +56,32 @@ topSort edges =
 
     visit :: String -> State ChemicalState ()
     visit s = do
-      (visited, top) <- get
-      put (Map.insert s 0 visited, top)
+      (visited, x, top) <- get
+      put (Map.insert s 0 visited, x, top)
 
     addTop :: String -> State ChemicalState ()
     addTop s = do
-      (visited, top) <- get
-      put (visited, s:top)
+      (visited, x, top) <- get
+      put (visited, x, s:top)
   in dfs "ORE"
 
 computeQuantity :: Edges -> String -> State ChemicalState ()
 computeQuantity edges s = do
-  (quantities, top) <- get
+  (quantities, quantitiesR, top) <- get
   let
     computeEdgeQ (v, (x, y)) = ceiling (fromIntegral vq / fromIntegral x) * y
       where vq = quantities Map.! v
 
+    computeEdgeQ' (v, (x, y)) = vq * (y % x)
+      where vq = quantitiesR Map.! v
+
     q = case edges Map.!? s of
             Nothing -> 1 -- Fuel
             Just edges' -> sum $ map computeEdgeQ edges'
-  put (Map.insert s q quantities, top)
+    q' = case edges Map.!? s of
+            Nothing -> 1 % 1 -- Fuel
+            Just edges' -> sum $ map computeEdgeQ' edges'
+  put (Map.insert s q quantities, Map.insert s q' quantitiesR, top)
 
 -- type Edges = Map.Map String [(String, (Integer, Integer))] -- u -> [(vi, (xi, yi))] where each xi vi = yi u + ...
 computeFuel :: Edges -> Integer -> State ChemicalState Integer
