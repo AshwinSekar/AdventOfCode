e8a3bb0bbf7eb2ea5515688d10455e871e1e2b91
Ashwin Sekar <gaurjas@gmail.com>
2020-06-03 19:28:37 -0400

    Solve 2019 Day 16 part 2

--- 2019/src/16-flawed-frequency-transmission.hs
+++ 2019/src/16-flawed-frequency-transmission.hs
@@ -14,8 +14,9 @@ main = do
   let signal = map digitToInt input
       ffts = iterate fft signal
       p1 = join $ map show (ffts !! 100)
+      p2 = join $ map show (findMessage input)
   putStrLn $ "Part 1: " ++ take 8 p1
-  -- putStrLn $ "Part 2: " ++ show p2
+  putStrLn $ "Part 2: " ++ p2
 
 
 fft :: [Int] -> [Int]
@@ -29,3 +30,18 @@ withPattern i s j = s * ([0, 1, 0, -1] !! ((j `div` i) `rem` 4))
 
 compute :: [Int] -> Int -> Int
 compute signal i = sum $ zipWith (withPattern i) signal [1..]
+
+findMessage :: String -> [Int]
+findMessage input =
+  let signal = map digitToInt input
+      len = 10000 * length signal
+      offset = read $ take 7 input
+      signal' = drop offset $ take len (cycle signal)
+      ffts = iterate fft' signal'
+  in  take 8 $ ffts !! 100
+
+fft' :: [Int] -> [Int]
+fft' signal =
+  let total = sum signal
+      signal' = map (total -) $ scanl (+) 0 signal
+  in init $ map (abs . (`rem` 10)) signal'
