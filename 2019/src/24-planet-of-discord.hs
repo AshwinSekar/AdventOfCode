59bc31123ac781bdb728359c70efc41416ad49b0
Ashwin Sekar <gaurjas@gmail.com>
2020-06-09 03:38:34 -0400

    Solve 2019 Day 24 part 2

--- 2019/src/24-planet-of-discord.hs
+++ 2019/src/24-planet-of-discord.hs
@@ -3,23 +3,70 @@ import Control.Monad
 
 import Data.Array
 import Data.Char
+import Data.Function
 import Data.List
+import Data.Maybe
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 import Utils
 import Debug.Trace
 
+type Bugs = Map.Map (Int, Int) Int
 
 main :: IO ()
 main = do
   putStrLn "Input:"
   input <- readLines []
   let bugs = concatMap parse input
+      bugs' = parseBug input
       l = listArray ((0, 0), (4, 4)) bugs
-      l' = findRepeat l (Set.singleton l)
-      p1 = rating l'
+      p1 = rating $ findRepeat l (Set.singleton l)
+      p2 = iterate step bugs'
+              & (!! 200)
+              & sum
   putStrLn $ "Part 1: " ++ show p1
-  putStrLn $ showBugs l'
+  putStrLn $ "Part 2: " ++ show p2
+
+
+parseBug :: [String] -> Bugs
+parseBug inputs = concat inputs
+  & foldl build (1, empty)
+  & snd
+  where build (13, bugs) _ = (14, bugs)
+        build (i, bugs) '.' = (i + 1, Map.insert (i, 0) 0 bugs)
+        build (i, bugs) '#' = (i + 1, Map.insert (i, 0) 1 bugs)
+        empty = Map.fromList [((i, z), 0) | i <- grid, z <- [-100..100]]
+        grid = delete 13 [1..25]
+
+step :: Bugs -> Bugs
+step bugs = Map.mapWithKey (transform' bugs) bugs
+
+transform' bugs (i, z) 0
+  | (adjs bugs (i, z) == 1) || (adjs bugs (i, z) == 2) = 1
+  | otherwise = 0
+transform' bugs (i, z) 1
+  | adjs bugs (i, z) == 1 = 1
+  | otherwise = 0
+
+adjs bugs (i, z) = adjs' bugs (i, z)
+  & mapMaybe (`Map.lookup` bugs)
+  & sum
+
+adjs' bugs (1, z) = [(8, z - 1), (12, z - 1), (2, z), (6, z)]
+adjs' bugs (5, z) = [(8, z - 1), (14, z - 1), (4, z), (10, z)]
+adjs' bugs (21, z) = [(12, z - 1), (18, z - 1), (16, z), (22, z)]
+adjs' bugs (25, z) = [(14, z - 1), (18, z - 1), (20, z), (24, z)]
+adjs' bugs (8, z) = [(3, z), (7, z), (9, z)] ++ map (, z + 1) [1..5]
+adjs' bugs (12, z) = [(7, z), (11, z), (17, z)] ++ map (\ i -> (5 * i + 1, z + 1)) [0..4]
+adjs' bugs (14, z) = [(9, z), (15, z), (19, z)] ++ map (\ i -> (5 * i, z + 1)) [1..5]
+adjs' bugs (18, z) = [(17, z), (19, z), (23, z)] ++ map (, z + 1) [21..25]
+adjs' bugs (i, z)
+  | i < 5 = [(8, z - 1), (i - 1, z), (i + 1, z), (i + 5, z)]
+  | i > 21 = [(18, z - 1), (i - 1, z), (i + 1, z), (i - 5, z)]
+  | i `rem` 5 == 1 = [(12, z - 1), (i - 5, z), (i + 5, z), (i + 1, z)]
+  | i `rem` 5 == 0 = [(14, z - 1), (i - 5, z), (i + 5, z), (i - 1, z)]
+  | otherwise = [(i - 5, z), (i + 5, z), (i - 1, z), (i + 1, z)]
 
 parse :: String -> [Int]
 parse = map parse'
