ef0efc688f671bf6580d26de5fbc6ce1bdf48b05
Ashwin Sekar <gaurjas@gmail.com>
2020-06-21 16:48:43 -0400

    Solve 2018 Day 16 part 2

--- 2018/src/16-chronal-classification.hs
+++ 2018/src/16-chronal-classification.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns #-}
 import Utils
 
 import Control.Monad
@@ -6,8 +6,11 @@ import Control.Monad
 import Data.Bits
 import Data.Function
 import Data.Functor
+import Data.List hiding ( (\\) )
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Set (Set, (\\))
+import qualified Data.Set as Set
 import Data.Void
 
 import System.IO
@@ -16,69 +19,104 @@ import Text.Megaparsec
 import Text.Megaparsec.Char (space, newline, string)
 import Text.Megaparsec.Char.Lexer (signed, decimal)
 
+import Debug.Trace
+
 type Parser = ParsecT Void String IO
 
-parser :: Parser [Sample]
-parser = (`sepEndBy` newline) $ Sample
-  <$> (string "Before: [" *> regParser <* string "]" <* newline)
-  <*> (decimal <* space) <*> (decimal <* space) <*> (decimal <* space) <*> (decimal <* newline)
-  <*> (string "After:  [" *> regParser <* string "]" <* newline)
+sampleParser :: Parser Sample
+sampleParser = Sample
+  <$> between (string "Before: [") (string "]") regParser <* newline
+  <*> instrParser <* newline
+  <*> between (string "After:  [") (string "]") regParser <* newline
 
 regParser :: Parser Registers
 regParser = decimal `sepEndBy` string ", "
              <&> zip [0..]
              <&> Map.fromList
 
+instrParser :: Parser Instr
+instrParser = Instr
+  <$> decimal <* space
+  <*> decimal <* space
+  <*> decimal <* space
+  <*> decimal
+
+parser :: Parser ([Sample], [Instr])
+parser = (,) <$> sampleParser `sepEndBy1` newline <* many newline
+             <*> instrParser `sepEndBy1` newline
+
+
+
 type Registers = Map Int Int
-type Opcode = Registers -> Int -> Int -> Int -> Registers
+data Opcode = ADDR | ADDI | MULR | MULI | BANR | BANI | BORR
+            | BORI | SETR | SETI | GTIR | GTRI | GTRR | EQIR | EQRI | EQRR
+            deriving (Enum, Bounded, Eq, Show, Ord)
+data Instr = Instr { op :: Int
+                   , a :: Int
+                   , b :: Int
+                   , c :: Int
+                   } deriving (Show, Eq)
 data Sample = Sample { r :: Registers
-                     , op :: Int
-                     , a :: Int
-                     , b :: Int
-                     , c :: Int
+                     , instr :: Instr
                      , r' :: Registers
                      } deriving (Show, Eq)
+
+
 
 main :: IO ()
 main = do
   putStrLn "Input:"
   input <- readFile "data/16-puzzle-input"
-  samples <- runParserT parser "" input >>= \case
-    Left e -> putStrLn (errorBundlePretty e) >> return []
-    Right s -> return s
-  putStrLn $ "Part 1: " ++ show (behave samples 3)
+  Right (samples, prog) <- runParserT parser "" input
+  let eval r Instr{op,a,b,c} = run (findCodings samples ! op) r a b c
+      r' = foldl eval (Map.fromList $ zip [0..] [0, 0, 0, 0]) prog
+  putStrLn $ "Part 1: " ++ show (valid samples 3)
+  putStrLn $ "Part 2: " ++ show (r' ! 0)
 
-behave :: [Sample] -> Int -> Int
-behave samples n = map behave' samples
-                    & filter ((>= n) . length)
-                    & length
+valid :: [Sample] -> Int -> Int
+valid samples n = map behave samples
+                   & filter ((>= n) . length)
+                   & length
 
-behave' :: Sample -> [Opcode]
-behave' Sample{r,a,b,c,r'} = do
-  op <- opcodes
-  guard (op r a b c == r')
+behave :: Sample -> [Opcode]
+behave Sample{r,instr,r'} = do
+  op <- [minBound..maxBound]
+  guard (run op r (a instr) (b instr) (c instr) == r')
   return op
 
+findCodings :: [Sample] -> Map Int Opcode
+findCodings samples = go Map.empty partial
+  where behaves = map (\ s -> (op $ instr s, Set.fromList $ behave s)) samples
+        partial = Map.fromListWith Set.intersection behaves
 
-opcodes :: [Opcode]
-opcodes = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+go :: Map Int Opcode -> Map Int (Set Opcode) -> Map Int Opcode
+go ops ints
+  | Map.null ints = ops
+  | otherwise = go ops' ints''
+  where (dones, ints') = Map.partition ((== 1) . Set.size) ints
+        ops' = Map.union ops $ Map.map (head . Set.toList) dones
+        taken = Set.fromList $ Map.elems ops'
+        ints'' = Map.map (\\ taken) ints'
 
-addr r a b c = Map.insert c (r ! a + r ! b) r
-addi r a b c = Map.insert c (r ! a + b) r
-mulr r a b c = Map.insert c (r ! a * r ! b) r
-muli r a b c = Map.insert c (r ! a * b) r
-banr r a b c = Map.insert c (r ! a .&. r ! b) r
-bani r a b c = Map.insert c (r ! a .&. b) r
-borr r a b c = Map.insert c (r ! a .|. r ! b) r
-bori r a b c = Map.insert c (r ! a .|. b) r
-setr r a _ c = Map.insert c (r ! a) r
-seti r a _ c = Map.insert c a r
-gtir r a b c = Map.insert c (test $ a > r ! b) r
-gtri r a b c = Map.insert c (test $ r ! a > b) r
-gtrr r a b c = Map.insert c (test $ r ! a > r ! b) r
-eqir r a b c = Map.insert c (test $ a == r ! b) r
-eqri r a b c = Map.insert c (test $ r ! a == b) r
-eqrr r a b c = Map.insert c (test $ r ! a == r ! b) r
+
+
+run :: Opcode -> Registers -> Int -> Int -> Int -> Registers
+run ADDR r a b c = Map.insert c (r ! a + r ! b) r
+run ADDI r a b c = Map.insert c (r ! a + b) r
+run MULR r a b c = Map.insert c (r ! a * r ! b) r
+run MULI r a b c = Map.insert c (r ! a * b) r
+run BANR r a b c = Map.insert c (r ! a .&. r ! b) r
+run BANI r a b c = Map.insert c (r ! a .&. b) r
+run BORR r a b c = Map.insert c (r ! a .|. r ! b) r
+run BORI r a b c = Map.insert c (r ! a .|. b) r
+run SETR r a _ c = Map.insert c (r ! a) r
+run SETI r a _ c = Map.insert c a r
+run GTIR r a b c = Map.insert c (test $ a > r ! b) r
+run GTRI r a b c = Map.insert c (test $ r ! a > b) r
+run GTRR r a b c = Map.insert c (test $ r ! a > r ! b) r
+run EQIR r a b c = Map.insert c (test $ a == r ! b) r
+run EQRI r a b c = Map.insert c (test $ r ! a == b) r
+run EQRR r a b c = Map.insert c (test $ r ! a == r ! b) r
 
 test :: Bool -> Int
 test b = if b then 1 else 0
