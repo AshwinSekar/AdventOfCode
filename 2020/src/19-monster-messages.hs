53b50cd24f8e1976b29559980257d64413fd3a43
Ashwin Sekar <gaurjas@gmail.com>
2020-12-22 17:02:35 -0500

    Finish solving 2020 Day 19

--- 2020/src/19-monster-messages.hs
+++ 2020/src/19-monster-messages.hs
@@ -1,40 +1,37 @@
 import Utils
 
 import Control.Applicative (liftA2)
-import Data.Foldable
-import Data.Function ((&))
+import Control.Monad.Extra (whenM)
 import Data.Functor
-import Data.Map ((!))
+import Data.Foldable
 import Data.Maybe
+import Data.Map ((!))
 import Data.Void
 import qualified Data.Map as Map
 import Text.Megaparsec hiding (count)
-import Text.Megaparsec.Char (letterChar, newline, char, string)
-import qualified Text.Megaparsec.Char.Lexer as L (decimal)
-import Text.Megaparsec.Debug
+import Text.Megaparsec.Char (letterChar, newline, char)
 
 ruleParser :: Parser (Int, Either Char [[Int]])
-ruleParser = liftA2 (,) (decimal <* char ':')
+ruleParser = liftA2 (,) (decimal <* symbol ":")
                         (choice [
-                          Left  <$> between (string " \"") (char '\"') letterChar <* newline,
-                          Right <$> some (someTill (char ' ' *> L.decimal) (string " |" <|> (newline $> [])))
+                          Left  <$> between (symbol "\"") (symbol "\"") letterChar,
+                          Right <$> some (try $ decimal <* notFollowedBy (symbol ":")) `sepBy` symbol "|"
                          ])
 
-parser = liftA2 (,) (Map.fromList <$> some ruleParser <* newline)
+parser = liftA2 (,) (Map.fromList <$> some ruleParser)
                     (some letterChar `sepEndBy1` newline)
 
 mkParser :: Map.Map Int (Parsec Void String ()) -> Either Char [[Int]] -> Parsec Void String ()
-mkParser rulesP (Left c) = dbg "Lit" $ char c $> ()
-mkParser rulesP (Right sub) = dbg "Pipe" (asum [traverse_ (rulesP !) s | s <- sub])
+mkParser rulesP (Left c) = char c $> ()
+mkParser rulesP (Right sub) = choice [try $ traverse_ (rulesP !) s | s <- sub]
 
 
 main :: IO ()
 main = do
   (rules, msgs)  <- parseFile "data/19-puzzle-input" parser
   let rulesP  = Map.map (mkParser rulesP) rules
-      rulesP' = Map.map (mkParser rulesP') $ Map.insert 8 (Right [[42], [42,8]]) (Map.insert 11 (Right [[42,31],[42,11,31]]) rules)
+      rule0   = whenM (liftA2 (<=) (length <$> some (try (rulesP ! 42))) (length <$> some (rulesP ! 31))) (fail "")
       p1 = count (isJust . parseMaybe (rulesP ! 0)) msgs
-      p2 = filter (isJust . parseMaybe (rulesP' ! 0)) msgs
-  print rulesP'
-  -- putStrLn $ "Part 1: " ++ show p1
+      p2 = count (isJust . parseMaybe rule0) msgs
+  putStrLn $ "Part 1: " ++ show p1
   putStrLn $ "Part 2: " ++ show p2
