e0e10074c76c89c37be6ec29938d84f0d7810afe
Ashwin Sekar <gaurjas@gmail.com>
2020-12-28 16:24:07 -0500

    Solve 2020 Day 25

---
+++ 2020/src/25-combo-breaker.hs
@@ -0,0 +1,17 @@
+import Utils
+
+import Data.Bits
+import Data.List
+
+transform :: Integer -> Integer -> Integer
+transform sn ls = sn `modPow` ls
+  where m = 20201227
+        modPow b 0 = 1
+        modPow b e = t * modPow ((b * b) `mod` m) (shiftR e 1) `mod` m
+           where t = if testBit e 0 then b `mod` m else 1
+
+main :: IO ()
+main = do
+  let (card, door) = (11239946, 10464955)
+      Just ls = find ((==card) . transform 7) [1..]
+  print $ transform door ls
