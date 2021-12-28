cd0ec2623a5018616c6cf74af773aae0b970100f
Ashwin Sekar <ashwin@solana.com>
2021-12-27 23:04:10 -0500

    Solve 2021 Day 3 Part 2

--- 2021/src/3-binary-diagnostic.hs
+++ 2021/src/3-binary-diagnostic.hs
@@ -14,9 +14,19 @@ main :: IO ()
 main = do
   input <- map (map digitToInt) <$> getFile "data/3-puzzle-input"
   let n = length input `div` 2
+      m = (length . head) input
       sums = map ((> n) . sum) $ transpose input
-      gamma = bin2dec $ map (bool 0 1) sums
-      eps = bin2dec $ map (bool 1 0) sums
-      p1 = gamma * eps
+      gamma = map (bool 0 1) sums
+      eps = map (bool 1 0) sums
+      p1 = bin2dec gamma * bin2dec eps
+      f _ [x] _ = [x]
+      f cmp nums i = filter (\x -> x !! i == b) nums
+        where
+          ones = sum $ map (!! i) nums
+          zeros = length nums - ones
+          b = bool 0 1 (cmp ones zeros)
+      [oxygen] = foldl (f (>=)) input [0 .. m]
+      [co2] = foldl (f (<)) input [0 .. m]
+      p2 = bin2dec oxygen * bin2dec co2
   putStrLn $ "Part 1: " ++ show p1
-  -- putStrLn $ "Part 2: " ++ show p2
+  putStrLn $ "Part 2: " ++ show p2
