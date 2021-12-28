ad9818074c6b25f26be7dd19584b208b512989b8
Ashwin Sekar <ashwin@solana.com>
2021-12-27 23:53:54 -0500

    Solve 2021 Day 6

---
+++ 2021/src/6-lanternfish.hs
@@ -0,0 +1,21 @@
+import           Utils
+
+import           Control.Applicative (liftA2, (<|>))
+
+import           Data.Function       ((&))
+import           Data.Map            ((!))
+import qualified Data.Map            as Map
+import qualified Data.Set            as Set
+
+import           Text.Megaparsec     (sepBy1)
+
+main :: IO ()
+main = do
+  fish <- parseFile "data/6-puzzle-input" $ decimal `sepBy1` symbol ","
+  let fishes = map (\x -> count (== x) fish) [0 .. 6]
+      babies = take 9 [0,0 ..]
+      go (a:fish, b:baby) = (fish ++ [a + b], baby ++ [a + b])
+      (f1, b1) = iterate go (fishes, babies) !! 80
+      (f2, b2) = iterate go (fishes, babies) !! 256
+  putStrLn $ "Part 1: " ++ show (sum f1 + sum b1)
+  putStrLn $ "Part 2: " ++ show (sum f2 + sum b2)
