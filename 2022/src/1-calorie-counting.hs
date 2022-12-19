cdb66544ef20cba9aac4e87cf0910867f6cf080b
Ashwin Sekar <ashwin@solana.com>
2022-12-19 14:35:46 -0500

    Solve 2022 day 1

---
+++ 2022/src/1-calorie-counting.hs
@@ -0,0 +1,21 @@
+import Utils ( parseFile )
+
+import           Control.Applicative (liftA2)
+import           Data.Map            ((!))
+import qualified Data.Map            as Map
+import qualified Data.Set            as Set
+import Data.List (sort)
+
+import Text.Megaparsec (some)
+import Text.Megaparsec.Char (newline, space)
+import Text.Megaparsec.Char.Lexer as L (decimal)
+
+main :: IO ()
+main = do
+  input <- parseFile "data/1-puzzle-input" $ some (some (L.decimal <* newline) <* space)
+  let calories = reverse . sort $ map sum input
+      p1 = head calories
+      p2 = sum $ map (calories !!) [0, 1, 2]
+
+  putStrLn $ "Part 1: " ++ show p1
+  putStrLn $ "Part 2 " ++ show p2
